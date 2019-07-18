1b598cd67b47fb0abdb96bfb9604233e1fa1425e
Celal Cankaya <celalcankaya@outlook.com>
2019-07-18 19:50:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  @@song_count = 0
+  @@count = 0
   @@genres = []
   @@artists = []
 
@@ -12,11 +12,11 @@ class Song
     @genre = genre
     @@genres << @genre
     @@artists << @artist
-    @@song_count +=1
+    @@count +=1
   end
 
   def self.count
-    @@song_count
+    @@count
   end
 
   def self.genres
